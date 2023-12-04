8e25d70b39f057666474c35ef642e083f3006916
jrf00020 <jrf00020@red.ujaen.es>
2023-12-04 18:27:27 +0100

    Add flexbox layout to App.css and update footer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,19 @@ function App() {
       <header>
         <h1>Cooperativa El Olivo Seco</h1>
         <h2>Monitorización de la producción de aceite</h2>
+        <hr />
         <div className="buttons">
           <DataButton active={lastData} data='Tiempo Real' onClick={() => setLastData(true)} />
           <DataButton active={!lastData} data='Histórico' onClick={() => setLastData(false)} />
         </div>
       </header>
-      <hr />
       <section className="data">
         {lastData ? <DataRealTime /> : <DataHistorical />}
       </section>
       <footer>
         <p>David Cruz Negrillo - Joaquín Rodríguez Ferrer - Andrés Serrano Barajas </p>
-          <p>2023 - Cooperativa El Olivo Seco S.L.</p>
+        <hr />
+        <p>2023 - Universidad de Jaén</p>
       </footer>
     </>
   )
